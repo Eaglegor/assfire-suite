0ffa44945a3a30a564fae710e88ddf7b257238c1
Nikolay Gorbachenko <eaglegor@gmail.com>
2021-02-22 19:46:29 +0300

    #00 Fixed (hopefully) linux tests compatibility

--- apps/router/engine/tests/OsrmReplies.hpp
+++ apps/router/engine/tests/OsrmReplies.hpp
@@ -7,7 +7,7 @@
 
 namespace assfire::router::test::osrm_replies {
 
-    std::wstring buildRequest(const Location &origin, const Location &destination, const std::string &overview) {
+    utility::string_t buildRequest(const Location &origin, const Location &destination, const std::string &overview) {
         std::string result;
         result += "/route/v1/driving/";
         result += std::to_string(origin.getLongitude().doubleValue()) + ",";
@@ -19,8 +19,8 @@ namespace assfire::router::test::osrm_replies {
         return convert(result);
     }
 
-    std::wstring createHint(const Location& loc) {
-        return L"AYa5kXOGuZEYAAAAJgAAAAAAAAAAAAAASjFaQU1xpUEAAAAAAAAAAAwAAAATAAAAAAAAAAAAAABXGAEA_kvMAKlYIQM8TMwArVghAwAA7wodsCRh";
+    utility::string_t createHint(const Location& loc) {
+        return convert("AYa5kXOGuZEYAAAAJgAAAAAAAAAAAAAASjFaQU1xpUEAAAAAAAAAAAwAAAATAAAAAAAAAAAAAABXGAEA_kvMAKlYIQM8TMwArVghAwAA7wodsCRh");
     }
 
 
@@ -29,47 +29,47 @@ namespace assfire::router::test::osrm_replies {
 
         value result;
 
-        result[L"code"] = value::string(L"Ok");
+        result[convert("code")] = value::string(convert("Ok"));
 
-        value& waypoints = result[L"waypoints"] = value::array(2);
+        value& waypoints = result[convert("waypoints")] = value::array(2);
 
         value& json_origin = waypoints[0];
-        json_origin[L"hint"] = value::string(createHint(origin));
-        json_origin[L"distance"] = 0;
-        value& json_origin_location = json_origin[L"location"] = value::array(2);
+        json_origin[convert("hint")] = value::string(createHint(origin));
+        json_origin[convert("distance")] = 0;
+        value& json_origin_location = json_origin[convert("location")] = value::array(2);
         json_origin_location[0] = origin.getLongitude().doubleValue();
         json_origin_location[1] = origin.getLatitude().doubleValue();
-        json_origin[L"name"] = value::string(L"Origin");
+        json_origin[convert("name")] = value::string(convert("Origin"));
 
         value& json_destination = waypoints[1];
-        json_destination[L"hint"] = value::string(createHint(destination));
-        json_destination[L"distance"] = 0;
-        value& json_destination_location = json_destination[L"location"] = value::array(2);
+        json_destination[convert("hint")] = value::string(createHint(destination));
+        json_destination[convert("distance")] = 0;
+        value& json_destination_location = json_destination[convert("location")] = value::array(2);
         json_destination_location[0] = destination.getLongitude().doubleValue();
         json_destination_location[1] = destination.getLatitude().doubleValue();
-        json_destination[L"name"] = value::string(L"Destination");
+        json_destination[convert("name")] = value::string(convert("Destination"));
 
-        value& routes = result[L"routes"] = value::array(1);
+        value& routes = result[convert("routes")] = value::array(1);
         value& route = routes[0];
 
-        value& legs = route[L"legs"] = value::array(1);
+        value& legs = route[convert("legs")] = value::array(1);
         value& leg = legs[0];
 
-        leg[L"steps"] = value::array();
-        leg[L"weight"] = (int32_t) route_details.getDuration().toSeconds();
-        leg[L"distance"] = route_details.getDistance().toMeters();
-        leg[L"duration"] = (int32_t) route_details.getDuration().toSeconds();
-        leg[L"summary"] = value();
+        leg[convert("steps")] = value::array();
+        leg[convert("weight")] = (int32_t) route_details.getDuration().toSeconds();
+        leg[convert("distance")] = route_details.getDistance().toMeters();
+        leg[convert("duration")] = (int32_t) route_details.getDuration().toSeconds();
+        leg[convert("summary")] = value();
 
-        route[L"weight_name"] = value::string(L"routability");
-        route[L"weight"] = (int32_t) route_details.getDuration().toSeconds();
-        route[L"distance"] = route_details.getDistance().toMeters();
-        route[L"duration"] = (int32_t) route_details.getDuration().toSeconds();
+        route[convert("weight_name")] = value::string(convert("routability"));
+        route[convert("weight")] = (int32_t) route_details.getDuration().toSeconds();
+        route[convert("distance")] = route_details.getDistance().toMeters();
+        route[convert("duration")] = (int32_t) route_details.getDuration().toSeconds();
 
         if (!route_details.getWaypoints().empty()) {
-            value &geometry = route[L"geometry"];
-            geometry[L"type"] = value::string(L"LineString");
-            value &coordinates = geometry[L"coordinates"] = value::array(route_details.getWaypoints().size());
+            value &geometry = route[convert("geometry")];
+            geometry[convert("type")] = value::string(convert("LineString"));
+            value &coordinates = geometry[convert("coordinates")] = value::array(route_details.getWaypoints().size());
             for (int i = 0; i < route_details.getWaypoints().size(); ++i) {
                 value &json_waypoint = coordinates[i] = value::array(2);
                 json_waypoint[0] = route_details.getWaypoints()[i].getLongitude().doubleValue();
