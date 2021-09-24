2312c0d1e0cca71772d4d6f5e4279312c9ef1627
Nikolay Gorbachenko <eaglegor@gmail.com>
2021-09-25 00:36:47 +0300

    #00 Added stubs for tsp proto translators

---
+++ apps/tsp/proto/assfire/api/v1/tsp/translators/TspPointTranslator.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+
+
+namespace assfire::tsp {
+    class TspPointTranslator {
+    public:
+        using ApiTspPoint =
+    };
+}
---
+++ apps/tsp/proto/assfire/api/v1/tsp/translators/TspTaskTranslator.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace assfire::tsp {
+    class TspTaskTranslator {
+
+    };
+}
