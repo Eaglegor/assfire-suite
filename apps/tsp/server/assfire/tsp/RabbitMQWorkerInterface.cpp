278616ab0086b28cedbd60113ff1b4a4c4380e3e
Nikolay Gorbachenko <eaglegor@gmail.com>
2021-10-19 10:31:16 +0300

    #00 Uncommented code

--- apps/tsp/server/assfire/tsp/RabbitMQWorkerInterface.cpp
+++ apps/tsp/server/assfire/tsp/RabbitMQWorkerInterface.cpp
@@ -10,14 +10,14 @@ namespace assfire::tsp::worker {
     RabbitMQWorkerInterface::RabbitMQWorkerInterface() :
             connection(nullptr),
             channel(&connection) {
-//        channel.declareQueue(TASK_QUEUE_NAME);
-//        channel.declareQueue(CONTROL_QUEUE_NAME);
-//        channel.declareQueue(RESULT_QUEUE_NAME);
+        channel.declareQueue(TASK_QUEUE_NAME);
+        channel.declareQueue(CONTROL_QUEUE_NAME);
+        channel.declareQueue(RESULT_QUEUE_NAME);
     }
 
     void RabbitMQWorkerInterface::publishNewTask(const WorkerTask &task) {
         std::string message = task.SerializeAsString();
-//        channel.publish(TASK_QUEUE_NAME, "my-key", message.c_str());
+        channel.publish(TASK_QUEUE_NAME, "my-key", message.c_str());
     }
 
     void RabbitMQWorkerInterface::publishPauseEvent(const std::string &task_id) {
@@ -25,7 +25,7 @@ namespace assfire::tsp::worker {
         signal.set_signal_type(WorkerControlSignal::WORKER_CONTROL_SIGNAL_TYPE_PAUSE);
         signal.set_task_id(task_id);
         std::string message = signal.SerializeAsString();
-//        channel.publish(CONTROL_QUEUE_NAME, "my-key", message.c_str());
+        channel.publish(CONTROL_QUEUE_NAME, "my-key", message.c_str());
     }
 
     void RabbitMQWorkerInterface::publishStopEvent(const std::string &task_id) {
@@ -33,7 +33,7 @@ namespace assfire::tsp::worker {
         signal.set_signal_type(WorkerControlSignal::WORKER_CONTROL_SIGNAL_TYPE_INTERRUPT);
         signal.set_task_id(task_id);
         std::string message = signal.SerializeAsString();
-//        channel.publish(CONTROL_QUEUE_NAME, "my-key", message.c_str());
+        channel.publish(CONTROL_QUEUE_NAME, "my-key", message.c_str());
     }
 
     void RabbitMQWorkerInterface::publishResumeEvent(const std::string &task_id) {
@@ -41,6 +41,6 @@ namespace assfire::tsp::worker {
         signal.set_signal_type(WorkerControlSignal::WORKER_CONTROL_SIGNAL_TYPE_RESUME);
         signal.set_task_id(task_id);
         std::string message = signal.SerializeAsString();
-//        channel.publish(CONTROL_QUEUE_NAME, "my-key", message.c_str());
+        channel.publish(CONTROL_QUEUE_NAME, "my-key", message.c_str());
     }
 }
