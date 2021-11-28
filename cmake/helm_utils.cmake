e827de124a9619807af76b20a36b226b2438db18
Nikolay Gorbachenko <eaglegor@gmail.com>
2021-11-28 19:27:43 +0300

    #21 Fixed secrets

--- cmake/helm_utils.cmake
+++ cmake/helm_utils.cmake
@@ -52,7 +52,7 @@ function(define_helm_target)
 
     add_custom_target(${TARGET_NAME}-upload
             DEPENDS ${TARGET_NAME}
-            COMMAND ${CURL_EXECUTABLE} --request POST --user gitlab-ci-token:$ENV{CI_JOB_TOKEN} --form "chart=@${CHART_NAME}-${CHART_VERSION}.tgz" "$ENV{CI_API_V4_URL}/projects/$ENV{CI_PROJECT_ID}/packages/helm/api/stable/charts"
+            COMMAND ${CURL_EXECUTABLE} --request POST --user gitlab-ci-token:$CI_JOB_TOKEN --form "chart=@${CHART_NAME}-${CHART_VERSION}.tgz" "$ENV{CI_API_V4_URL}/projects/$ENV{CI_PROJECT_ID}/packages/helm/api/stable/charts"
             WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${CHART_ROOT}
             COMMENT "Uploading helm chart ${CHART_NAME}-${CHART_VERSION}.tgz to package repository $ENV{CI_API_V4_URL}/projects/$ENV{CI_PROJECT_ID}/packages/helm/api/stable/charts"
             )
@@ -72,7 +72,7 @@ function(define_helm_target)
 
     add_custom_target(${TARGET_NAME}-deploy
             DEPENDS ${TARGET_NAME}
-            COMMAND ${HELM_EXECUTABLE} repo add --username $ENV{CI_REGISTRY_USER} --password $ENV{CI_REGISTRY_PASSWORD} assfire-suite $ENV{CI_API_V4_URL}/projects/$ENV{CI_PROJECT_ID}/packages/helm/api/stable
+            COMMAND ${HELM_EXECUTABLE} repo add --username $CI_REGISTRY_USER --password $CI_REGISTRY_PASSWORD assfire-suite $ENV{CI_API_V4_URL}/projects/$ENV{CI_PROJECT_ID}/packages/helm/api/stable
             COMMAND ${HELM_EXECUTABLE} upgrade -i -n ${ASSFIRE_HELM_INSTALL_RELEASE_NAMESPACE} ${INSTALL_RELEASE_NAME} assfire-suite/${CHART_NAME} --version "${ASSFIRE_APPLICATION_RELEASE_VERSION}"
             WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${CHART_ROOT}
             COMMENT "Uninstalling helm release ${INSTALL_RELEASE_NAME}, release namespace: ${ASSFIRE_HELM_INSTALL_RELEASE_NAMESPACE}"
