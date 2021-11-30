4c0a5fe6ed3074562859e93ac3095a94e7865aaa
Nikolay Gorbachenko <eaglegor@gmail.com>
2021-12-01 01:11:38 +0300

    #22 Added unimplemented stub for locations server

--- apps/locations/go/server/server.go
+++ apps/locations/go/server/server.go
@@ -5,27 +5,22 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"log"
 	"net"
 	"time"
 )
 
 var (
-	bindAddress    = flag.String("bind-address", "localhost", "Server bind address")
-	port           = flag.Int("port", 50051, "Server port")
-	redisEndpoint  = flag.String("redis-endpoint", "localhost", "Redis endpoint")
-	redisPort      = flag.Int("redis-port", 6379, "Redis port")
-	redisPassword  = flag.String("redis-password", "", "Redis password")
-	redisDatabase  = flag.Int("redis-database", 0, "Redis database id")
-	rabbitEndpoint = flag.String("rabbit-endpoint", "localhost", "Rabbit MQ endpoint")
-	rabbitPort     = flag.Int("rabbit-port", 5672, "Rabbit MQ port")
-	rabbitUser     = flag.String("rabbit-user", "guest", "Rabbit MQ user")
-	rabbitPassword = flag.String("rabbit-password", "guest", "Rabbit MQ password")
+	bindAddress = flag.String("bind-address", "localhost", "Server bind address")
+	port        = flag.Int("port", 50051, "Server port")
+	mongoDbHost = flag.String("mongodb-host", "localhost", "MongoDB host")
+	mongoDbPort = flag.Int("mongodb-port", 27017, "MongoDB port")
 )
 
 type LocationsServer struct {
@@ -34,34 +29,56 @@ type LocationsServer struct {
 	mongoClient *mongo.Client
 }
 
-func main() {
-	flag.Parse()
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	log.Printf("Trying to connect to mongodb")
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+func (server *LocationsServer) GetLocations(ctx context.Context, request *locations.GetLocationsRequest) (*locations.GetLocationsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetLocations not implemented")
+}
+func (server *LocationsServer) SaveLocations(ctx context.Context, request *locations.SaveLocationsRequest) (*locations.SaveLocationsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SaveLocations not implemented")
+}
+func (server *LocationsServer) RemoveLocations(ctx context.Context, request *locations.RemoveLocationsRequest) (*locations.RemoveLocationsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method RemoveLocations not implemented")
+}
+func (server *LocationsServer) FindLocations(ctx context.Context, request *locations.FindLocationsRequest) (*locations.FindLocationsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method FindLocations not implemented")
+}
+func (server *LocationsServer) GenerateLocations(ctx context.Context, request *locations.GenerateLocationsRequest) (*locations.GenerateLocationsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GenerateLocations not implemented")
+}
+func (server *LocationsServer) ResolveAddresses(ctx context.Context, request *locations.ResolveAddressesRequest) (*locations.ResolveAddressesResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ResolveAddresses not implemented")
+}
+
+func createMongoClient(ctx context.Context) *mongo.Client {
+	mongoConnectionString := fmt.Sprintf("mongodb://%s:%d", *mongoDbHost, *mongoDbPort)
+	log.Printf("Trying to connect to mongoDB at %s", mongoConnectionString)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoConnectionString))
 	if err != nil {
-		log.Fatalf("Failed to connect to mongodb: %v", err)
+		log.Fatalf("Failed to connect to mongoDB: %v", err)
 	}
 
+	log.Printf("Checking if mongoDB client connected...")
 	err = client.Ping(ctx, readpref.Primary())
 	if err != nil {
-		log.Fatalf("Failed to connect to mongodb: %v", err)
+		log.Fatalf("Failed to connect to mongoDB: %v", err)
 	}
+	return client
+}
 
-	collection := client.Database("testing").Collection("numbers")
-	one, err := collection.InsertOne(ctx, bson.D{{"name", "pi"}, {"value", 3.14}})
+func destroyMongoClient(ctx context.Context, client *mongo.Client) {
+	err := client.Disconnect(ctx)
 	if err != nil {
-		log.Fatalf("Failed to insert: %v", err)
+		log.Fatalf("Failed to disconnect from mongo: %v", err)
 	}
+}
 
-	log.Printf("Inserted: %v", one.InsertedID)
+func main() {
+	flag.Parse()
 
-	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
-			log.Fatalf("Failed to disconnect from mongo: %v", err)
-		}
-	}()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	mongoClient := createMongoClient(ctx)
+	defer destroyMongoClient(ctx, mongoClient)
 
 	log.Printf("Starting listening at %s:%d\n", *bindAddress, *port)
 	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", *bindAddress, *port))
@@ -72,7 +89,7 @@ func main() {
 	grpcServer := grpc.NewServer(opts...)
 	log.Println("Creating server...")
 	server := &LocationsServer{
-		mongoClient: client,
+		mongoClient: mongoClient,
 	}
 	log.Println("Registering server handlers...")
 	locations.RegisterLocationsServiceServer(grpcServer, server)
