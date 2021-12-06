55d2927efe9c595fb0c19de52c78e13e6dbd4443
Nikolay Gorbachenko <eaglegor@gmail.com>
2021-12-06 22:20:09 +0300

    #22 Removed (yet) unimplemented methods from locations service. Will implement them when they will be needed

--- apps/locations/go/server/server.go
+++ apps/locations/go/server/server.go
@@ -6,41 +6,19 @@ import (
 	"flag"
 	"fmt"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 	"log"
 	"math/rand"
 	"net"
-	"time"
 )
 
 var (
 	bindAddress = flag.String("bind-address", "localhost", "Server bind address")
 	port        = flag.Int("port", 50051, "Server port")
-	mongoDbHost = flag.String("mongodb-host", "localhost", "MongoDB host")
-	mongoDbPort = flag.Int("mongodb-port", 27017, "MongoDB port")
 )
 
 type LocationsServer struct {
 	locations.UnimplementedLocationsServiceServer
-
-	mongoClient *mongo.Client
-}
-
-func (server *LocationsServer) GetLocations(ctx context.Context, request *locations.GetLocationsRequest) (*locations.GetLocationsResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetLocations not implemented")
-}
-func (server *LocationsServer) SaveLocations(ctx context.Context, request *locations.SaveLocationsRequest) (*locations.SaveLocationsResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method SaveLocations not implemented")
-}
-func (server *LocationsServer) RemoveLocations(ctx context.Context, request *locations.RemoveLocationsRequest) (*locations.RemoveLocationsResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method RemoveLocations not implemented")
-}
-func (server *LocationsServer) FindLocations(ctx context.Context, request *locations.FindLocationsRequest) (*locations.FindLocationsResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method FindLocations not implemented")
 }
 
 func (server *LocationsServer) GenerateLocations(ctx context.Context, request *locations.GenerateLocationsRequest) (*locations.GenerateLocationsResponse, error) {
@@ -61,26 +39,6 @@ func (server *LocationsServer) GenerateLocations(ctx context.Context, request *l
 	}, nil
 }
 
-func (server *LocationsServer) ResolveAddresses(ctx context.Context, request *locations.ResolveAddressesRequest) (*locations.ResolveAddressesResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method ResolveAddresses not implemented")
-}
-
-func createMongoClient(ctx context.Context) *mongo.Client {
-	mongoConnectionString := fmt.Sprintf("mongodb://%s:%d", *mongoDbHost, *mongoDbPort)
-	log.Printf("Trying to connect to mongoDB at %s", mongoConnectionString)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoConnectionString))
-	if err != nil {
-		log.Fatalf("Failed to connect to mongoDB: %v", err)
-	}
-
-	log.Printf("Checking if mongoDB client connected...")
-	err = client.Ping(ctx, readpref.Primary())
-	if err != nil {
-		log.Fatalf("Failed to connect to mongoDB: %v", err)
-	}
-	return client
-}
-
 func destroyMongoClient(ctx context.Context, client *mongo.Client) {
 	err := client.Disconnect(ctx)
 	if err != nil {
@@ -91,12 +49,6 @@ func destroyMongoClient(ctx context.Context, client *mongo.Client) {
 func main() {
 	flag.Parse()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	mongoClient := createMongoClient(ctx)
-	defer destroyMongoClient(ctx, mongoClient)
-
 	log.Printf("Starting listening at %s:%d\n", *bindAddress, *port)
 	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", *bindAddress, *port))
 	if err != nil {
@@ -105,9 +57,7 @@ func main() {
 	var opts []grpc.ServerOption
 	grpcServer := grpc.NewServer(opts...)
 	log.Println("Creating server...")
-	server := &LocationsServer{
-		mongoClient: mongoClient,
-	}
+	server := &LocationsServer{}
 	log.Println("Registering server handlers...")
 	locations.RegisterLocationsServiceServer(grpcServer, server)
 
